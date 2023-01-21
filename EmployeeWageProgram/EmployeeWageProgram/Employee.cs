14e9845ae33ce65b07f07372c560ef9f4774889c
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-21 17:17:49 +0530

    [Prashant]Adding: Calculating Daily Wage of Part time Employee

--- EmployeeWageProgram/EmployeeWageProgram/Employee.cs
+++ EmployeeWageProgram/EmployeeWageProgram/Employee.cs
@@ -11,24 +11,31 @@ namespace EmployeeWageProgram
         public void EmployeeWage()
         {
             //Constants
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if(empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
-                Console.WriteLine("\nEmployee is Present");
+                Console.WriteLine("\nFull Time Employee");
+            }
+            else if(empCheck == IS_PART_TIME) 
+            {
+                empHrs = 4;
+                Console.WriteLine("\nPart Time Employee");
             }
             else
             {
                 empHrs = 0;
                 Console.WriteLine("\nEmployee is Absent");
             }
+             
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
