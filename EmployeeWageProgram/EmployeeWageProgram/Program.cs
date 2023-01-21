fd6fe9aba2e0bc816c537a1d663dd7a2e2353b29
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-21 16:47:50 +0530

    [Prashant] Check : Employee Present or Absent

---
+++ EmployeeWageProgram/EmployeeWageProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeWageProgram;
+using System;
+namespace EmployeeWgeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Employee employee = new Employee();
+            employee.EmployeeWage();
+        }
+    }
+}
