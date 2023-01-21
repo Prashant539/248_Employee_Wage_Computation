de8144bf6550fe3d70fda84977a0df017fa88f12
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-01-21 17:07:32 +0530

    [Prashant]Adding : Employee Daily Wage

--- EmployeeWageProgram/EmployeeWageProgram/Employee.cs
+++ EmployeeWageProgram/EmployeeWageProgram/Employee.cs
@@ -12,17 +12,25 @@ namespace EmployeeWageProgram
         {
             //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
             if(empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+                Console.WriteLine("\nEmployee is Present");
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+                Console.WriteLine("\nEmployee is Absent");
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
        
     }
